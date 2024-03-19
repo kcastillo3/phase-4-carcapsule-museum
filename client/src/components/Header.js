5f8c14f96f4f6667be920e4ad0cd7ac071b8cdf6
Desiah Barnett <desiahbarnett@gmail.com>
2024-03-19 19:08:10 -0400

    added image source to header component and css... import NavBar child...

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,17 @@
+// Header.js
+
 import React from 'react';
-import NavBar from './NavBar';
+import NavBar from 'NavBar';
 
 const Header = () => {
-  return (
-    <header>
-      <NavBar />
-    </header>
-  );
-}
+return (
+<header>
+    <div className="container">
+    <img src="/path/to/logo.png" alt="Car Capsule Museum Logo" className="logo" />
+    <NavBar />
+    </div>
+</header>
+);
+};
 
 export default Header;
