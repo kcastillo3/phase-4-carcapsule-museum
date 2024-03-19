0e217e677fdce317a00edbe501a051bdb6e7f49d
Desiah Barnett <desiahbarnett@gmail.com>
2024-03-19 18:24:17 -0400

    useraccount fetches reviews from backend. API endpoint is established fetching the user review and displaying them

---
+++ client/src/components/UserAccount.js
@@ -0,0 +1,15 @@
+// UserAccount.js
+import React from 'react';
+
+const UserAccount = () => {
+  return (
+    <div>
+      {/* Title */}
+      <h2>User Account</h2>
+      {/* Display user account details */}
+      {/* Add your user account details display code here */}
+    </div>
+  );
+}
+
+export default UserAccount;
