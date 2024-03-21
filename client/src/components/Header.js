b48ae04b7e9c16c9fd37c9f4627ceb9a60b08c24
ckorley4 <ckorley4uo@yahoo.com>
2024-03-21 07:39:51 -0400

    More CSS

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,32 @@
 // Header.js
 
-import React from 'react';
-import NavBar from './Navbar';
+import React from 'react'
+import NavBar from './Navbar'
+import '../index.css'
+import logo from '../../src/logo192.png'
 
 const Header = () => {
-return (
-<header>
-    <div className="container">
-    <img src="/path/to/logo.png" alt="Car Capsule Museum Logo" className="logo" />
-    <NavBar />
-    </div>
-</header>
-);
-};
+  return (
+    <header>
+      <div
+        className="container"
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          boxShadow: '1px 1px 3px #333',
+          fontSize: '20px',
+        }}
+      >
+        <img
+          style={{ height: '60px', width: '60px' }}
+          src={logo}
+          alt="Car Capsule Museum Logo"
+          className="logo"
+        />
+        <NavBar />
+      </div>
+    </header>
+  )
+}
 
-export default Header;
+export default Header
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../index.css'
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/car">Car Page</Link></li>
-        <li><Link to="/login">Login</Link></li>
+    <nav classNmae="navbar">
+      <ul
+        style={{
+          display: 'flex',
+          margin: '0.5rem',
+          listStyle: 'none',
+          justifyContent: 'space-between',
+          padding: '5px',
+          width: '200px',
+        }}
+      >
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/car">Car Page</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
       </ul>
     </nav>
-  );
+  )
 }
 
-export default NavBar;
+export default NavBar
