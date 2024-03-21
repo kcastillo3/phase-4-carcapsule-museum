75d739892bbaf497f587fa80f4585cb50e216e60
ckorley4 <ckorley4uo@yahoo.com>
2024-03-21 02:55:52 -0400

    Made some changes to CSS

--- client/src/components/ReviewForm.js
+++ client/src/components/ReviewForm.js
@@ -1,20 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const ReviewForm = ({ onSubmitReview }) => {
-  const [content, setContent] = useState('');
+  const [content, setContent] = useState('')
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    onSubmitReview(content);
-    setContent('');
+    e.preventDefault()
+
+    setContent('')
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <textarea value={content} onChange={(e) => setContent(e.target.value)} placeholder="Write your review..." required></textarea>
-      <button type="submit">Submit Review</button>
-    </form>
-  );
+    <div>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          placeholder="Write your review..."
+          required
+        ></textarea>
+        <button type="submit">Submit Review</button>
+      </form>
+    </div>
+  )
 }
 
-export default ReviewForm;
+export default ReviewForm
