b280c8bcb8d5d7b3f5b3296bc8bcfdd01324ff50
batshevaparshan <batshevabp@gmail.com>
2024-03-21 18:02:18 -0400

    users

--- server/models.py
+++ server/models.py
@@ -1,6 +1,18 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
-
 from config import db
 
-# Models go here!
+Base = declarative_base()
+
+class Users(Base, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(255), nullable=False, unique=True)
+    username = Column(String(100), nullable=False, unique=True)
+    password_hash = Column(String(255), nullable=False)
+
+    def __repr__(self):
+        return f"<User(id={self.id}, email={self.email}, username={self.username})>"
