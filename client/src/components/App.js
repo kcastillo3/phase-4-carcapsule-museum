9a791d678d3ad977d7e704f275281fa4ad09a260
Desiah Barnett <desiahbarnett@gmail.com>
2024-03-22 03:32:09 -0400

    css styling

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -29,7 +29,7 @@ const App = () => {
 
   return (
     <Router>
-      <div className="App">
+      <div className="app">
         <Header isLoggedIn={isLoggedIn} onLogout={handleLogout} />
         <Switch>
           <Route exact path="/" component={Home} />
