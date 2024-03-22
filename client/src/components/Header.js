c867c6b5ccda7c66e61d4cbbae8f6e1d7c0b2f55
Desiah Barnett <desiahbarnett@gmail.com>
2024-03-21 21:01:46 -0400

    header css created and nav bar wrapper

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,18 +2,21 @@
 
 import React from 'react';
 import Navbar from './Navbar'; 
+import '../index.css'; // Import the CSS file for styling
+
 
 // Updated Header to accept isLoggedIn and onLogout props
 const Header = ({ isLoggedIn, onLogout }) => {
   return (
-  <header>
-    <div className="container">
-      <img src="/path/to/logo.png" alt="Car Capsule Museum Logo" className="logo" />
-      {/* Pass isLoggedIn and onLogout to NavBar */}
-      <Navbar isLoggedIn={isLoggedIn} onLogout={onLogout} />
+    <header className="header">
+      <div className="container">
+        <img src="https://res.cloudinary.com/doyp4tk82/image/upload/v1711064951/Purple_Badge_Car_Wash_Logo_uphfrk.jpg" alt="Car Capsule Museum Logo" className="logo" />
+        <div className="navbar-wrapper">
+          <Navbar isLoggedIn={isLoggedIn} onLogout={onLogout} />
+        </div>
       </div>
-      </header>
-      );
-    };
+    </header>
+  );
+};
 
 export default Header;
